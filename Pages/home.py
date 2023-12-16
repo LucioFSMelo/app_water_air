f17b8bb65785a846c2aff7e916410c8fc986cd9f
LuciofsMelo <lucius.mat@gmail.com>
2023-12-16 09:16:20 -0300

    Atualizando o texto

--- Pages/home.py
+++ Pages/home.py
@@ -18,32 +18,26 @@ def home():
     
     st.markdown(""" Em um mundo onde as cidades lutam para respirar, onde a água que bebemos é um reflexo de nossas ações, nasceu a PuraVida Solutions,  
                 uma empresa dedicada a transformar a qualidade do ar que respiramos e a água que bebemos. 
-                   
                 **Problemática Observada**  
                 :blue[**Nas movimentadas ruas de Recife**] onde a qualidade do ar atingia os níveis mais baixos, indicando um alerta para a saúde da população.  
                 As pessoas sentiam a necessidade de uma mudança, e foi aí que a PuraVida entrou em cena. Com uma visão clara de criar um ambiente mais saudável,  
                 a empresa concentrou seus esforços na descontaminação da atmosfera.  
                 :blue[**Ao mesmo tempo, na cidade de Abreu e Lima**], a qualidade do ar era notavelmente superior. PuraVida estudou as práticas ambientais dessa  
                 cidade, aprendendo lições valiosas sobre o que pode ser feito para alcançar um ar mais limpo.  
-                  
                 **Inspiração**  
                 Entendendo que a qualidade do ar está intrinsecamente ligada à saúde de nossos corpos, a PuraVida expandiu seu compromisso para além dos céus poluídos.  
                 Em San Francisco, onde as águas eram mais límpidas, a empresa encontrou inspiração para enfrentar a poluição da água. Por outro lado, em Berlin, onde a  
                 taxa de poluição da água era mais alta, PuraVida viu um desafio a ser superado.  
-                  
                 **Argumentação**
                 Nossa jornada nos levou a explorar a relação entre a qualidade do ar e a poluição da água. Será que cidades com ar puro também cuidam melhor de suas  
                 fontes de água?  
-                  
                 **Aqui começa nossa jornada**
                 A PuraVida mergulhou fundo nessa questão, analisando dados em busca de padrões e respostas.  
-                  
                 **Ação**  
                 Viajamos por todo o globo, agrupando dados por país e região. Descobrimos que diferentes áreas geográficas abordam questões ambientais de maneiras únicas.  
                 A qualidade do ar e a poluição da água tornaram-se um reflexo das políticas ambientais e das práticas de gestão.  
                 Em nossa jornada, identificamos cidades que se destacam, seja pela positiva ou negativa. Lugares onde o ar é notavelmente puro e a água impecável, e  
                 outros que enfrentam desafios significativos. Cada descoberta nos incentivou a avançar.  
-                  
                 **Realizações**  
                 Visualizando essas diferenças, criamos mapas e dashboards para compartilhar nosso conhecimento e conscientizar o mundo. Queremos que todos possam enxergar  
                 a importância de cuidar do nosso planeta e inspirar mudanças significativas.  
